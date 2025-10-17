4b209528ec04886381ceae4b46aa095948bd7bea
Fatma-Ozturk <fatmabuyukkozturk@gmail.com>
2025-10-17 15:19:57 +0300

    Remove old mobile_app folder after rename to site_app

--- mobile_app/android/app/src/main/kotlin/com/example/site_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.site_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- mobile_app/lib/core/config.dart
+++
@@ -1,4 +0,0 @@
-class AppConfig {
-  static const bool useMockApi = false; // ✅ ARTIK GERÇEK API
-  static const String apiBaseUrl = 'https://192.168.1.55:8080'; // ← SENİN BASE URL’İN
-}
